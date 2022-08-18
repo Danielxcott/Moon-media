157c468f45ccf5c02f0026a9425e32e72305fcd4
Danielxcott <changwilliam826@gmail.com>
2022-08-18 21:40:18 +0630

    added detail page with reaction viewer and comment section

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,30 @@ const nav = document.querySelector(".nav-menu"),
             toggleBtn.addEventListener("click",()=>{
                 nav.classList.toggle("open")
             })
+function commentBtn(x)
+{
+    $(".comment-asidenav").toggleClass("active");
+}
+
+$(".comment-aside-close").click(function(){
+    $(".comment-asidenav").toggleClass("active");
+})
+$(".close-com-btn").click(function(){
+    $(".comment-asidenav").toggleClass("active");
+})
+
+function viewerBtn(x)
+{
+    const viewer = $("#viewers"+x);
+    const close = $("#viewerClose"+x);
+    viewer.toggleClass("active");
+    close.addClass("active");
+}
+
+function closeReaction(x)
+{
+    const closeBtn = $("#viewerClose"+x);
+    const viewer = $("#viewers"+x);
+    viewer.toggleClass("active");
+    closeBtn.removeClass("active");
+}
