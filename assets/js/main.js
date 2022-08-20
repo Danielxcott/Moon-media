d8413f446449575dc4ada2961e040aa7519ec069
Danielxcott <changwilliam826@gmail.com>
2022-08-20 23:48:56 +0630

    added responsive for frontend page and fixed dashboard detail post and profile page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,44 @@
 $(".search-btn").click(function(){
     $(".input-box").toggleClass("active");
 });
-// $('.category-lists').slick({
-//     infinite: false,
-//     slidesToShow: 3,
-//     slidesToScroll: 3
-//   });
+try{
+    $('.category-lists-mobile').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 1008,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 800,
+              settings: 'unslick',
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                },
+              },
+          ],
+      });
+}catch(error)
+{
+    console.warn("slick carousel is not support here")
+}
 
 const nav = document.querySelector(".nav-menu"),
             toggleBtn = nav.querySelector(".toggle-btn");
